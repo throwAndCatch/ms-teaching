8f9079020ed679f0997f6ad8a650f84cbdc02ab4
Superzp <programmer.zhoup@gmail.com>
2020-11-26 19:57:47 +0800

    springcloud bus

---
+++ config-client-3334/src/main/java/com/demo/controller/ConfigController.java
@@ -0,0 +1,22 @@
+package com.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+@RequestMapping("/config")
+public class ConfigController {
+
+    @Value("${test.version}")
+    private String version;
+
+    @GetMapping("/get")
+    public String getVersion(){
+        return version;
+
+    }
+}
